513c1a52913a545f978796e17b40efbab18c5e08
surayyaaliyu <159139574+surayyaaliyu@users.noreply.github.com>
2024-03-23 01:08:20 +0200

    Bullet now effective and functional

---
+++ bullet.h
@@ -0,0 +1,32 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QImage>
+#include "player.h"
+
+class Bullet : public QObject, public QGraphicsPixmapItem {
+    Q_OBJECT
+
+
+public:
+    Bullet(const QPointF& pos, const QImage& image, QGraphicsItem* parent = nullptr);
+
+    Bullet();
+
+
+private:
+    QPointF m_position;
+    QImage m_image;
+    qreal m_speed;
+
+public slots:
+    void handleCollision();
+    void move();
+};
+
+
+
+
+#endif // BULLET_H
